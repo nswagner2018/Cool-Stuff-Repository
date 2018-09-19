44d40ec7a3e077761d6b4aa617dc6d42bb3d2c52
nswagner2018 <43423481+nswagner2018@users.noreply.github.com>
2018-09-19 17:26:07 -0400

    Add files via upload

---
+++ ec_1.c
@@ -0,0 +1,16 @@
+//Nathan Wagner
+//ec_1.c
+//Extra Credit 1
+//21 September 2018
+// Yeahh.
+
+#include <stdio.h>
+
+int main(void)
+{
+	int std;
+
+	printf("Please Enter an Integer Here: ");
+	scanf("%d", &std);
+	printf("%d", std);
+}
